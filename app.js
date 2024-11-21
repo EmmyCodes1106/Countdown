254d0b76e66b37a7653bb1c09c6a446977b8cc1f
Fadayomi Emmanuel <138496573+EmmyCodes1106@users.noreply.github.com>
2024-11-21 15:35:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set the target date and time
-const targetDate = new Date("Decrmber 25, 2024 14:05:00").getTime();
+const targetDate = new Date("Decrmber 25, 2024 00:00:00").getTime();
 
 // Get HTML elements for countdown updates
 const daysElement = document.getElementById("days");
